a1b069544f1c7173b5637cd42d2adc73b45592cb
malripo <malripo741205@gmail.com>
2025-02-01 15:58:27 +0900

    Initial commit

---
+++ a_core/urls.py
@@ -0,0 +1,54 @@
+"""a_core URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+# from a_posts.views import home_view
+from a_posts.views import *
+from a_users.views import *
+# for media
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # for theboss
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    # path('admin/', admin.site.urls),
+    path('theboss/', admin.site.urls),
+    # for allauth
+    path('accounts/', include('allauth.urls')),
+    path('', home_view, name='home'),
+    path('category/<tag>/', home_view, name='category'),
+    path('post/create/', post_create_view, name='post-create'),
+    path('post/delete/<pk>/', post_delete_view, name='post-delete'),
+    path('post/edit/<pk>/', post_edit_view, name='post-edit'),
+    path('post/<pk>/', post_page_view, name='post'),
+    path('post/like/<pk>/', like_post, name="like-post"), 
+    path('comment/like/<pk>/', like_comment, name="like-comment"), 
+    path('reply/like/<pk>/', like_reply, name="like-reply"),
+    # path('category/<tag>/', category_view, name='category'),
+    path('profile/', profile_view, name="profile"),
+    path('<username>/', profile_view, name="userprofile"),  
+    path('profile/edit/', profile_edit_view, name="profile-edit"),
+    path('profile/delete/', profile_delete_view, name="profile-delete"),
+    path('profile/onboarding/', profile_edit_view, name="profile-onboarding"),
+    path('profile/verify-email/', profile_verify_email, name="profile-verify-email"),
+    path('commentsent/<pk>/', comment_sent, name="comment-sent"),
+    path('comment/delete/<pk>/', comment_delete_view, name='comment-delete'),  
+    path('replysent/<pk>/', reply_sent, name='reply-sent'),  
+    path('reply/delete/<pk>/', reply_delete_view, name='reply-delete'),  
+]
+# for media
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ a_users/views.py
@@ -0,0 +1,97 @@
+# from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.db.models import Count
+from django.contrib import messages
+# from django.contrib.sites.models import Site
+from django.http import Http404
+from django.contrib.auth.models import User
+from allauth.account.utils import send_email_confirmation
+from a_posts.forms import ReplyCreateForm
+# from a_inbox.forms import InboxNewMessageForm
+from .forms import *
+
+# Create your views here.
+
+# def profile_view(request):
+def profile_view(request, username=None):
+
+    # profile = request.user.profile
+    
+    if username:
+        profile = get_object_or_404(User, username=username).profile
+    else:
+        try:
+            profile = request.user.profile
+        except:
+            raise Http404()
+        
+    posts = profile.user.posts.all()
+    
+    if request.htmx:
+        if 'top-posts' in request.GET:
+            posts = profile.user.posts.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+            # return render(request, 'snippets/loop_profile_posts.html', { 'posts': posts })
+        elif 'top-comments' in request.GET:
+            comments = profile.user.comments.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+            replyform = ReplyCreateForm()
+            return render(request, 'snippets/loop_profile_comments.html', { 'comments': comments, 'replyform': replyform })
+        elif 'liked-posts' in request.GET:
+            posts = profile.user.likedposts.order_by('-likedpost__created') 
+        return render(request, 'snippets/loop_profile_posts.html', { 'posts': posts })
+    
+    # new_message_form = InboxNewMessageForm()
+    
+    context = {
+        'profile' : profile,
+        'posts': posts,
+        # 'new_message_form' : new_message_form
+    }
+
+    # return render(request, 'a_users/profile.html')    
+    return render(request, 'a_users/profile.html', context)
+
+
+@login_required
+def profile_edit_view(request):
+    form = ProfileForm(instance=request.user.profile)
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            # return redirect('profile')
+            
+            if request.user.emailaddress_set.get(primary=True).verified:
+                return redirect('profile')
+            else:
+                return redirect('profile-verify-email') 
+        
+    if request.path == reverse('profile-onboarding'):
+        template = 'a_users/profile_onboarding.html'
+    else:
+        template = 'a_users/profile_edit.html'
+
+    # return render(request, 'a_users/profile_edit.html')
+    # return render(request, 'a_users/profile_edit.html', {'form':form})          
+    return render(request, template, {'form':form})
+
+
+@login_required
+def profile_delete_view(request):
+    user = request.user
+    
+    if request.method == 'POST':
+        logout(request)
+        user.delete()
+        messages.success(request, 'Account deleted, what a pity')
+        return redirect('home')
+    
+    return render(request, 'a_users/profile_delete.html' )
+
+
+def profile_verify_email(request):
+    send_email_confirmation(request, request.user)
+    return redirect('profile')
