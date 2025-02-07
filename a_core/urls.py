7da884f19d94f5aa0eda0f5e05ca21a5c425e44d
malripo <malripo741205@gmail.com>
2025-02-07 12:46:31 +0900

    path delete

--- a_core/urls.py
+++ a_core/urls.py
@@ -55,7 +55,6 @@ urlpatterns = [
     # path('category/<tag>/', category_view, name='category'),
     path('profile/', profile_view, name="profile"),
     path('_/', include('a_landingpages.urls')), 
-    path('<username>/', profile_view, name="userprofile"),
     path('<username>/', profile_view, name="userprofile"),  
     path('profile/edit/', profile_edit_view, name="profile-edit"),
     path('profile/delete/', profile_delete_view, name="profile-delete"),
